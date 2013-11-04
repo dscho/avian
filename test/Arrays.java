c0eb1f44c83023b0d466d7d8ed43616c61a1282e
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-11-04 14:26:17 -0600

    Failed attempt at an in-place mergesort
    The idea was as following: when two sorted arrays
    
    	a[0]..a[n] b[0]..b[n]
    
    are merged into c[...], we could keep a[...] as two pieces, the top and
    then the bottom one:
    
    	c[0]..c[m] a[k]..a[n] a[i]..a[k-1] b[j]..b[n]
    
    However, the problematic scenario is when a[i] is smaller than b[j]: we
    would need to shift a[i+1]..a[k-1] one to the left to make it work, and
    that is just not doable if we want to keep O(n log n) worst case runtime.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- test/Arrays.java
+++ test/Arrays.java
@@ -3,6 +3,80 @@ public class Arrays {
     if (! v) throw new RuntimeException();
   }
 
+  private static <T extends Comparable<T>> void mergeSort(T[] array) {
+//System.err.println(java.util.Arrays.toString(array));
+     for (int stride = 1; stride < array.length; stride <<= 1) {
+//System.err.println("=== stride: " + stride);
+      for (int begin = 0; begin < array.length; ) {
+        int i1 = begin, i2 = begin + stride, end = i2 + stride;
+        if (end >= array.length) {
+          if (i2 >= array.length) {
+            break;
+          }
+          end = array.length;
+        }
+
+        int i;
+        for (i = begin; i1 < i2 && i2 < end; ++i) {
+          T a = array[i1], b = array[i2];
+System.err.print("                 ");
+for (int j = i1; j < i2; ++j) {
+  System.err.print(", " + array[j]);
+}
+for (int j = i; j < i1; ++j) {
+  System.err.print(", " + array[j]);
+}
+System.err.print("    -----    ");
+for (int j = i2; j < end; ++j) {
+  System.err.print(", " + array[j]);
+}
+System.err.println();
+if (i1 == 0 && i2 == 2) {
+  System.err.println("Hello");
+}
+expect(i <= i1);
+expect(i1 < i2);
+expect (i == begin || array[i].compareTo(array[i - 1]) >= 0);
+expect(i1 + 1 < i2 ? array[i1].compareTo(array[i1 + 1]) <= 0 : (i1 == i || array[i1].compareTo(array[i]) <= 0));
+expect(i2 + 1 >= end || array[i2].compareTo(array[i2 + 1]) <= 0);
+System.err.println("i: " + i + ", i1: " + i1 + ", i2: " + i2 + ", end: " + end);
+System.err.println("" + a + " <= " + b + ": " + a.compareTo(b));
+          if (a.compareTo(b) <= 0) {
+            if (i1 == i) {
+              ++i1;
+            } else {
+              array[i1] = array[i];
+              array[i] = a;
+              if (i1 + 1 < i2) {
+                if (++i1 == i2) {
+                  i1 = i + 1;
+                }
+              }
+            }
+          } else {
+            array[i2++] = array[i];
+            array[i] = b;
+            if (i == i1) {
+              i1 = i2 - 1;
+            }
+          }
+System.err.println(java.util.Arrays.toString(array));
+        }
+        if (i1 < i2) {
+          while (i < i1) {
+            T a = array[i1];
+            array[i1] = array[i];
+            array[i++] = a;
+            if (i1 + 1 < i2) {
+              i1++;
+            }
+          }
+        }
+        begin = end;
+      }
+    }
+  }
+
   private static <T extends Comparable<T>> void expectSorted(T[] array) {
     for (int i = 1; i < array.length; ++i) {
       expect(array[i - 1].compareTo(array[i]) <= 0);
@@ -39,6 +113,72 @@ public class Arrays {
   }
 
   public static void main(String[] args) {
+if (!true) {
+  int count = 8; // 60000;
+  int trials = 999;
+  Integer[] array = new Integer[count];
+  int random = 12345;
+
+  long best = Long.MAX_VALUE, worst = -Long.MAX_VALUE, average = 0;
+  for (int k = 0; k < trials; ++k) {
+System.err.println("k: " + k);
+    for (int i = 0; i < array.length; ++i) {
+      array[i] = random % 20;
+      random = pseudoRandom(random);
+    }
+System.err.println(java.util.Arrays.toString(array));
+    long start = System.nanoTime();
+    mergeSort(array);
+    long end = System.nanoTime();
+    long total = end - start;
+    if (best > total) {
+      best = total;
+    }
+    if (worst < total) {
+      worst = total;
+    }
+    average += total;
+System.err.println(java.util.Arrays.toString(array));
+    expectSorted(array);
+  }
+  average /= trials;
+
+  random = 12345;
+  long best2 = Long.MAX_VALUE, worst2 = 0, average2 = 0;
+  for (int k = 0; k < trials; ++k) {
+    for (int i = 0; i < array.length; ++i) {
+      array[i] = random;
+      random = pseudoRandom(random);
+    }
+    long start = System.nanoTime();
+    java.util.Arrays.sort(array);
+    long end = System.nanoTime();
+    long total = end - start;
+    if (best2 > total) {
+      best2 = total;
+    }
+    if (worst2 < total) {
+      worst2 = total;
+    }
+    average2 += total;
+    expectSorted(array);
+  }
+  average2 /= trials;
+
+  System.err.println("" + best2 + " - " + best + ": " + (best2 - best));
+  System.err.println("" + worst2 + " - " + worst + ": " + (worst2 - worst));
+  System.err.println("" + average2 + " - " + average + ": " + (average2 - average));
+  return;
+}
+if (true) {
+  Integer[] array = new Integer[] { 6, 5, 3, 1, 8, 7, 2, 4 };
+array = new Integer[] { -2, 0, 1, 3, -1, -3, 4, 2 };
+array = new Integer[] { 5, 11, 9, -17, -19, -1, 1, 3 };
+array = new Integer[] { 0, -2, 1, 2, -3, -1, -4, 3 };
+  mergeSort(array);
+  expectSorted(array);
+  return;
+}
     { int[] array = new int[0];
       Exception exception = null;
       try {
